2ab3851ddf422f88ed66140bb679a15bb57b8c64
Fathima Beegum P J <60310640+beegumfathima@users.noreply.github.com>
2022-10-25 17:24:24 +0530

    Hackaway_website

---
+++ js/session.js
@@ -0,0 +1,22 @@
+function onBtn1Press() {
+    console.log("Hai")
+    document.getElementById("card2").style.display="none"
+    document.getElementById("card3").style.display="none"
+    document.getElementById("card1").style.display="block"
+}
+
+function onBtn2Press() {
+    console.log("Hai")
+    document.getElementById("card1").style.display="none"
+    document.getElementById("card3").style.display="none"
+    document.getElementById("card2").style.display="block"
+}
+
+
+
+function onBtn3Press() {
+    console.log("Hai")
+    document.getElementById("card1").style.display="none"
+    document.getElementById("card2").style.display="none"
+    document.getElementById("card3").style.display="block"
+}
